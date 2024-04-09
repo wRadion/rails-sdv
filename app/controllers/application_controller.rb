9d1f80191ab648e04a74db914eb03cec0787176b
wRadion <wradion@gmail.com>
2024-04-09 14:35:59 +0200

    Fix path with lcoocale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale]
   end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
